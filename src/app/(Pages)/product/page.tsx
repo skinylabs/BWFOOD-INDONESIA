105261f68661c11e28fc28ce069103530a8636f6
skinylabs <skinylabs@gmail.com>
2024-08-29 15:53:41 +0700

    add product section

--- src/app/(Pages)/product/page.tsx
+++ src/app/(Pages)/product/page.tsx
@@ -1,14 +1,37 @@
+import MaxWidthWrapper from "@/components/Frontend/Layouts/MaxWidthWrapper";
 import PageBanner from "@/components/Frontend/Layouts/PageBanner";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Product() {
   return (
-    <div>
-      <PageBanner
-        imageSrc="/images/header/blue-pattern.svg"
-        heading="Produk"
-        height="h-96"
-      />
-      <h1>Product Page</h1>
-    </div>
+    <section className="w-full">
+      <div>
+        <PageBanner
+          imageSrc="/images/header/blue-pattern.svg"
+          heading="Produk"
+          height="h-96"
+        />
+        <h1>Product Page</h1>
+      </div>
+      <MaxWidthWrapper className="flex flex-col gap-20 mt-[26rem] py-12">
+        <div className="w-full h-full grid grid-cols-5 gap-8">
+          {Array.from({ length: 9 }).map((_, index) => (
+            <Link
+              href={"/"}
+              className="relative w-60 h-60 hover:scale-105 duration-300 transition-all ease-in-out cursor-pointer"
+              key={index}
+            >
+              <Image
+                src="/images/career.jpg"
+                alt="banner-image"
+                fill
+                className="object-cover object-center rounded-2xl"
+              />
+            </Link>
+          ))}
+        </div>
+      </MaxWidthWrapper>
+    </section>
   );
 }
